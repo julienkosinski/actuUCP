1e7f2008eb5c35c62e52f0fa5f9d4fce1cb43a7a
julienkosinski <julien.kosinski@gmail.com>
2014-09-29 11:22:26 +0200

    Ability change news to the new model newspaper.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -8,12 +8,12 @@ class Ability
     if user.has_role? :admin
         can :manage, :all
     elsif user.has_role? :association
-        can :create, :news
-        can [[:update, :delete], :news], News do |news|
+        can :create, :newspaper
+        can [[:update, :delete], :newspaper], News do |news|
             news.try(:user_id) == user.id
         end
     else
-        can :show, :news
+        can :show, :newspaper
     end
     #
     # The first argument to `can` is the action you are giving the user 
