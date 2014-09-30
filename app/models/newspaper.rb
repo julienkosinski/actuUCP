d7e4c014f83d944e8ff70ccebd1faf0e8d5beed7
julienkosinski <julien.kosinski@gmail.com>
2014-09-30 09:20:04 +0200

    Testing environment for twitter connect.

--- app/models/newspaper.rb
+++ app/models/newspaper.rb
@@ -1,20 +1,20 @@
 class Newspaper < ActiveRecord::Base
 	require 'twitter'
 	def twitterConnect
-		app_secret = YAML.load_file('app_secret.yml')
-		if app_secret == false
-			@client = Twitter::REST::Client.new do |config|
-				config.consumer_key        = ENV['twitter_consumer_key']
-				config.consumer_secret     = ENV['twitter_consumer_secret']
-				config.access_token        = ENV['twitter_access_token']
-				config.access_token_secret = ENV['twitter_access_token_secret']
-			end
-		else
+		if Rails.env == "development"
+			app_secret = YAML.load_file('app_secret.yml')
 			@client = Twitter::REST::Client.new do |config|
 				config.consumer_key        = app_secret['consumer_key']
 				config.consumer_secret     = app_secret['consumer_secret']
 				config.access_token        = app_secret['access_token']
 				config.access_token_secret = app_secret['access_token_secret']
+			end
+		else
+			@client = Twitter::REST::Client.new do |config|
+				config.consumer_key        = ENV['twitter_consumer_key']
+				config.consumer_secret     = ENV['twitter_consumer_secret']
+				config.access_token        = ENV['twitter_access_token']
+				config.access_token_secret = ENV['twitter_access_token_secret']
 			end
 		end
 	end
