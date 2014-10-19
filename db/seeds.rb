80c2d7c38076720b5729a65439f423c0f0853d83
Anne <pommefraise88@gmail.com>
2014-10-19 15:12:09 +0200

    Add default newspaper in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,5 @@ categories = Category.create([{ id: '1', name: 'Sport' }, { id: '2', name: 'Vie 
 	{ id: '3', name: 'Culture' }, { id: '4', name: 'Vie pratique' }, { id: '5', name: 'Sorties' },
 	{ id: '6', name: 'Recherche' }, { id: '7', name: 'Formations' },
 	{ id: '8', name: 'A la une' }])
+
+newspapers = Newspaper.create([{id: '1', title: "Le RCGennevilliers répond", description: "Après les succès du Real Cergy et du FC Paris samedi, le RCGennevilliers a lui aussi empoché trois points ce dimanche, lors de la 8e journée de Liga. Les Matelassiers l'ont emporté à domicile face au RC de Gennevilliers (2-0).", author: 'Michou', id_categories: '1'}])
