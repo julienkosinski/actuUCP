7a9c80a9734cef2ebfcc723754969db914e908d7
Clownyfunny <barbarin.pierre05@gmail.com>
2014-11-02 21:00:59 +0100

    Change default value of twitter

--- app/models/newspaper.rb
+++ app/models/newspaper.rb
@@ -4,15 +4,15 @@ class Newspaper < ActiveRecord::Base
 	def retrieveTweetFromAccountUCPCergy
 		#return $client.search("from:UniversiteCergy", :result_type => "recent").take(200).collect do |tweet|
 			#{}"#{tweet.user.screen_name}: #{tweet.text} #{tweet.created_at}"
-					return $client.search("from:UniversiteCergy", :result_type => "recent").take(200).collect
+					return $client.search("from:UniversiteCergy", :result_type => "recent").take(10).collect
 
 	end
 	def retrieveTweetFromAccountRPUCP
-		return $client.search("from:rp_ucp", :result_type => "recent").take(200).collect
+		return $client.search("from:rp_ucp", :result_type => "recent").take(10).collect
 			#{}"#{tweet.user.screen_name}: #{tweet.text}"
 	end
 	def retrieveTweetFromAccountUCPAssoLide
-		return $client.search("from:LIDEdeCergy", :result_type => "recent").take(200).collect
+		return $client.search("from:LIDEdeCergy", :result_type => "recent").take(10).collect
 			#{}"#{tweet.user.screen_name}: #{tweet.text}"
 	end
 end
