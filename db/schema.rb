83db65de131173ad1b9ad7c556e97781eaa1fafc
Samantha <you@example.com>
2014-11-02 14:56:30 +0100

    Inté header samantha

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140924144148) do
+ActiveRecord::Schema.define(version: 20140924131415) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,12 +42,6 @@ ActiveRecord::Schema.define(version: 20140924144148) do
 
   add_index "roles", ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id", using: :btree
   add_index "roles", ["name"], name: "index_roles_on_name", using: :btree
-
-  create_table "twitters", force: true do |t|
-    t.string   "searchElements"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
