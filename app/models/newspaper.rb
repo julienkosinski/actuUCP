22370138f568e419ae735e6d6b0c51192da18431
Anne <pommefraise88@gmail.com>
2014-11-02 15:37:36 +0100

    Format display tweet in view

--- app/models/newspaper.rb
+++ app/models/newspaper.rb
@@ -2,18 +2,17 @@ class Newspaper < ActiveRecord::Base
 	require 'twitter'
 
 	def retrieveTweetFromAccountUCPCergy
-		return $client.search("from:UniversiteCergy", :result_type => "recent").take(200).collect do |tweet|
-			"#{tweet.user.screen_name}: #{tweet.text} #{tweet.created_at}"
-		end
+		#return $client.search("from:UniversiteCergy", :result_type => "recent").take(200).collect do |tweet|
+			#{}"#{tweet.user.screen_name}: #{tweet.text} #{tweet.created_at}"
+					return $client.search("from:UniversiteCergy", :result_type => "recent").take(200).collect
+
 	end
 	def retrieveTweetFromAccountRPUCP
-		return $client.search("from:rp_ucp", :result_type => "recent").take(200).collect do |tweet|
-			"#{tweet.user.screen_name}: #{tweet.text}"
-		end
+		return $client.search("from:rp_ucp", :result_type => "recent").take(200).collect
+			#{}"#{tweet.user.screen_name}: #{tweet.text}"
 	end
 	def retrieveTweetFromAccountUCPAssoLide
-		return $client.search("from:LIDEdeCergy", :result_type => "recent").take(200).collect do |tweet|
-			"#{tweet.user.screen_name}: #{tweet.text}"
-		end
+		return $client.search("from:LIDEdeCergy", :result_type => "recent").take(200).collect
+			#{}"#{tweet.user.screen_name}: #{tweet.text}"
 	end
 end
